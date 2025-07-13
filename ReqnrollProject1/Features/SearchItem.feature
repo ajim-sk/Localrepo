65b5e72f9714c88f1c3f18889287379309aaf9e2
Ajim <ajim.shaikh0038@gmail.com>
2025-07-13 10:25:14 +0530

    Add files

---
+++ ReqnrollProject1/Features/SearchItem.feature
@@ -0,0 +1,10 @@
+Feature: Search functionality
+
+@Ajim	
+Scenario: user searching product
+Given User open chrome browser
+When User Enters URL
+When User Enters Product
+When User click on search button
+
+
---
+++ ReqnrollProject1/StepDefinitions/SearchStepDefinitions.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReqnrollProject1.StepDefinitions
+{
+    [Binding]
+    internal class SearchStepDefinitions
+    {
+        private IWebDriver driver;
+        [Given("User open chrome browser")]
+        public void GivenUserOpenChromeBrowser()
+        {
+            driver =new ChromeDriver();
+        }
+
+        [When("User Enters URL")]
+        public void WhenUserEntersURL()
+        {
+            driver.Url = "https://www.youtube.com/";
+        }
+
+        [When("User Enters Product")]
+        public void WhenUserEntersProduct()
+        {
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//*[@type='text']")).SendKeys("Ayesha Shaikh");
+                 
+        }
+
+        [When("User click on search button")]
+        public void WhenUserClickOnSearchButton()
+        {
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//*[@type='text']")).SendKeys(Keys.Enter);
+            driver.Quit();
+        }
+
+    }
+}
